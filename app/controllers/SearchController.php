3a78c6fc76f98b08d7f52c8a3fa77d23c24f8b03
Spike1953 <kevin.baland@he-arc.ch>
2014-12-03 22:08:10 +0100

    Controller de recherche

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -7,27 +7,20 @@ class SearchController extends BaseController {
 
         $query = Input::get("query");
 
-        //TODO: add snippets that match with the search
-        $searchResultSnippetData = array(
-            array(
-                "id" => "-1",
-                "Tri bulle en java",
-                "Java",
-                "17/11/2014"
-            ),
-            array(
-                "id" => "-1",
-                "Tri bulle en C",
-                "C",
-                "18/10/2014"
-            ),
-            array(
-                "id" => "-1",
-                "Division euclidienne en Python",
-                "Python",
-                "17/10/2014"
-            )
-        );
+        $tabSnippets = Snippet::where("name","like","%".$query."%")->get();
+        
+        $searchResultSnippetData = array();
+        $i = 0;
+        foreach ($tabSnippets as $snip) 
+        {
+             $searchResultSnippetData[$i] = array(
+                "id" => $snip->id,
+                $snip->name,
+                Langage::find($snip->langage_id)->name,
+                $snip->created_at
+             );
+            $i++;
+        }
 
         $searchResultsSnippetsTable = array(
             "tableTitle" => "",
