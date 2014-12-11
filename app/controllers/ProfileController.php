212415ffff3c0711276fe0e2d8753523b01d5ecc
Spike1953 <kevin.baland@he-arc.ch>
2014-12-11 14:25:10 +0100

    Affichage correct du profil par rapport a l'utilisateur connect

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -15,19 +15,26 @@ class ProfileController extends BaseController {
     public function show() {
         $languages = parent::getListLangage();
         
-        $profile = array(
-            "pseudo" => "toto",
-            "mail" => "toto@monmaildefou.com"
-        );
-
-        $data = array(
-            "languages" => $languages,
-            "profile" => $profile
-        );
-
-        return View::make('profile', $data);
+        if(Auth::check())
+        {
+            $user = Auth::user();
+            $profile = array(
+                "pseudo" => $user->pseudo,
+                "mail" =>  $user->email
+            );
+            
+            $data = array(
+                "languages" => $languages,
+                "profile" => $profile
+            );
+            return View::make('profile', $data);
+        }
+        else
+        {
+            return Redirect::to("login");
+        }
+        
     }
 
 }
-
 ?>
